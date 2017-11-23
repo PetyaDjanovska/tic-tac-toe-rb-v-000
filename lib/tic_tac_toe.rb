6dab73b25fd331ab47fc8a27868a851d0c66f3a5
Petya Djanovska <petya.djanovska@gmail.com>
2017-11-23 23:38:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,3 +8,11 @@ WIN_COMBINATIONS = [
   [0,4,8],  # left diagonal
   [2,4,6]  # right diagonal
 ]
+
+def display_board(array)
+puts " #{array[0]} | #{array[1]} | #{array[2]} "
+puts "-----------"
+puts " #{array[3]} | #{array[4]} | #{array[5]} "
+puts "-----------"
+puts " #{array[6]} | #{array[7]} | #{array[8]} "
+end
