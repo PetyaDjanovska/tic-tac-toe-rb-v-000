3959613b5ecca725700822c6f2d9b73c6a17363b
Petya Djanovska <petya.djanovska@gmail.com>
2017-11-24 00:37:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -133,14 +133,14 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board)
+  #if won?(board)
     win_combination = won?(board)
 
     if board[win_combination[0]] == "X"
       return "X"
     elsif board[win_combination[0]] == "O"
       return "O"
-    end
+    #end
   else
     nil
   end
