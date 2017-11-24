c539d5a48cbef564ea4c52ab286bdaa816cb26a2
Petya Djanovska <petya.djanovska@gmail.com>
2017-11-24 00:26:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,6 +56,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
+  current_player = current_player(board)
   if valid_move?(board,index)
     position_taken?(board,index)
     move(board,index,current_player)
